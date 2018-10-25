7d6c7c2531826b4f778ac377698eee6bbcb9aa91
FlameInTheDark <nin-vain@mail.ru>
2018-10-25 08:14:56 +0300

    modify script

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,5 @@
 go build
+docker kill $(docker ps -a -q --filter="name=dtbot")
+docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'imagename')
 docker build -t dtbot .
 docker run -d --rm -e BOT_TOKEN=$BOT_TOKEN --name dtbot dtbot:latest
