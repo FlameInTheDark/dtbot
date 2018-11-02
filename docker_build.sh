c37d3e1629eafa267123326bbb29ed46f0b02e2a
FlameInTheDark <nin-vain@mail.ru>
2018-11-02 15:02:15 +0500

    Fix command

--- docker_build.sh
+++ docker_build.sh
@@ -3,4 +3,4 @@ go build
 docker kill $(docker ps -a -q --filter="name=dtbot")
 docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'imagename')
 docker build -t dtbot .
-docker run -d --rm --autorestart ALWAYS -e BOT_TOKEN=$BOT_TOKEN -e MONGO_CONN=$MONGO_CONN --name dtbot dtbot:latest
+docker run -d --rm --restart ALWAYS -e BOT_TOKEN=$BOT_TOKEN -e MONGO_CONN=$MONGO_CONN --name dtbot dtbot:latest
