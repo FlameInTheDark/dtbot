35b92f108133344fcd5092a92daff5dc5527c755
FlameInTheDark <nin-vain@mail.ru>
2018-11-07 16:06:25 +0500

    Update build script

--- docker_build.sh
+++ docker_build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 go build
 docker kill $(docker ps -a -q --filter="name=dtbot")
+docker rm $(docker ps -a -q --filter="name=dtbot")
 docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'imagename')
 docker build -t dtbot .
 docker run -d --restart always -e BOT_TOKEN=$BOT_TOKEN -e MONGO_CONN=$MONGO_CONN --name dtbot dtbot:latest
