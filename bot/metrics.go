3ca5bd140aab390ecc1d855bffe71493e7a1ca10
FlameInTheDark <nin-vain@mail.ru>
2019-01-16 15:51:31 +0500

    Added metrics

--- bot/metrics.go
+++ bot/metrics.go
@@ -10,5 +10,9 @@ func (ctx *Context) MetricsCommand(command string) {
 	query := []byte(fmt.Sprintf("commands,server=%v command=%v", ctx.Guild.ID, command))
 	addr := fmt.Sprintf("%v/write?db=%v", ctx.Conf.Metrics.Address, ctx.Conf.Metrics.Database)
 	r := bytes.NewReader(query)
-	_, _ = http.Post(addr, "", r)
+	_, err := http.Post(addr, "", r)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
 }
