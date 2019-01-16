b1e66354dd628d3e0336667679627b377d6f0939
FlameInTheDark <nin-vain@mail.ru>
2019-01-16 15:53:52 +0500

    Added metrics

--- bot/metrics.go
+++ bot/metrics.go
@@ -7,6 +7,9 @@ import (
 )
 
 func (ctx *Context) MetricsCommand(command string) {
+	fmt.Println("Metrics!")
+	fmt.Println("Guild: " + ctx.Guild.ID)
+	fmt.Println("Command: " + command)
 	query := []byte(fmt.Sprintf("commands,server=%v command=%v", ctx.Guild.ID, command))
 	addr := fmt.Sprintf("%v/write?db=%v", ctx.Conf.Metrics.Address, ctx.Conf.Metrics.Database)
 	r := bytes.NewReader(query)
