77df28b7ee1ad8209d66f26fcd557ca4a95f8b2e
FlameInTheDark <nin-vain@mail.ru>
2019-03-15 10:14:52 +0500

    Update Twitch announcer

--- bot/twitch.go
+++ bot/twitch.go
@@ -91,7 +91,12 @@ func TwitchInit(session *discordgo.Session, conf *Config, db *DBWorker) *Twitch 
 		guildStreams := db.GetTwitchStreams(g.ID)
 		counter += len(guildStreams)
 		guilds[g.ID] = &TwitchGuild{g.ID, guildStreams}
+		fmt.Println("Bot:")
 		for _, gs := range guilds[g.ID].Streams {
+			fmt.Println(gs.Login, " : ", gs.Guild)
+		}
+		fmt.Println("Database:")
+		for _, gs := range guildStreams {
 			fmt.Println(gs.Login, " : ", gs.Guild)
 		}
 	}
