fea274778dbadfbd71c4dc47081b5a08225a03e8
FlameInTheDark <nin-vain@mail.ru>
2019-03-25 09:36:28 +0500

    Debug

--- bot/radio.go
+++ bot/radio.go
@@ -29,7 +29,7 @@ func (connection *Connection) Play(source string, volume float32) error {
 	if connection.playing {
 		return errors.New("song already playing")
 	}
-	ffmpeg := exec.Command("ffmpeg", "-i", source, "-f", "s16le", "-ar", strconv.Itoa(FRAME_RATE), "-ac", strconv.Itoa(CHANNELS), "pipe:1", /*"-filter:a", fmt.Sprintf("volume=%.2f", volume)*/)
+	ffmpeg := exec.Command("ffmpeg", "-i", source, "-f", "s16le", "-ar", strconv.Itoa(FRAME_RATE), "-ac", strconv.Itoa(CHANNELS), "pipe:1", fmt.Sprintf("-filter:a volume=%.2f", volume))
 	connection.stopRunning = false
 	out, err := ffmpeg.StdoutPipe()
 	if err != nil {
--- bot/song.go
+++ bot/song.go
@@ -1,6 +1,7 @@
 package bot
 
 import (
+	"fmt"
 	"os/exec"
 	"strconv"
 )
@@ -16,7 +17,7 @@ type Song struct {
 // Ffmpeg returns ffmpeg executable command
 func (song Song) Ffmpeg(volume float32) *exec.Cmd {
 	return exec.Command("ffmpeg", "-i", song.Media, "-f", "s16le", "-ar", strconv.Itoa(FRAME_RATE), "-ac",
-		strconv.Itoa(CHANNELS), "pipe:1", /*"-filter:a", fmt.Sprintf("volume=%.2f", volume)*/)
+		strconv.Itoa(CHANNELS), "pipe:1", fmt.Sprintf("-filter:a volume=%.2f", volume))
 }
 
 // NewSong creates and returns new song
