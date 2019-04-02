40561f902af73526921d20d7e8d8cef7bba53848
FlameInTheDark <nin-vain@mail.ru>
2019-04-02 16:47:03 +0500

    Fixed creating bad voice sessions

--- bot/session.go
+++ bot/session.go
@@ -84,11 +84,7 @@ func (manager *SessionManager) Join(discord *discordgo.Session, guildID, channel
 	properties JoinProperties, volume float32) (*Session, error) {
 	vc, err := discord.ChannelVoiceJoin(guildID, channelID, properties.Muted, properties.Deafened)
 	if err != nil {
-		if _, ok := discord.VoiceConnections[guildID]; ok {
-			vc = discord.VoiceConnections[guildID]
-		} else {
-			return nil, err
-		}
+		return nil, err
 	}
 	sess := newSession(guildID, channelID, NewConnection(vc), volume)
 	manager.sessions[channelID] = sess
