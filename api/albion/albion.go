09a690cea8f4c15f58a03ea24feefd9678aaee05
FlameInTheDark <nin-vain@mail.ru>
2019-04-30 22:57:14 +0500

    Fixes

--- api/albion/albion.go
+++ api/albion/albion.go
@@ -162,10 +162,12 @@ func ShowKills(ctx *bot.Context) {
 			embed.Color(ctx.GuildConf().EmbedColor)
 			for _, k := range kills {
 				fmt.Println("Killed " + k.Victim.Name)
-				t, err := time.Parse("2019-04-28T18:59:21.942273100Z", k.TimeStamp)
 				var timeString string
+				t, err := time.Parse("2019-04-28T18:59:21.942273100Z", k.TimeStamp)
 				if err == nil {
 					timeString = fmt.Sprintf("%v.%v.%v %v:%v", t.Day(), t.Month(), t.Year(), t.Hour(), t.Minute())
+				} else {
+					fmt.Println("Parse time: ", err.Error())
 				}
 				embed.Field(k.Victim.Name, fmt.Sprintf(ctx.Loc("albion_kill_short"), k.Victim.DeathFame, k.Victim.AverageItemPower, timeString), false)
 			}
