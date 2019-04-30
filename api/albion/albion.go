8a3f8f9bc36b78f24912322f7d63b420231d60b8
FlameInTheDark <nin-vain@mail.ru>
2019-04-30 23:33:47 +0500

    Fixes

--- api/albion/albion.go
+++ api/albion/albion.go
@@ -169,7 +169,7 @@ func ShowKills(ctx *bot.Context) {
 				} else {
 					fmt.Println("Parse time: ", err.Error())
 				}
-				embed.Field(k.Victim.Name, fmt.Sprintf(ctx.Loc("albion_kill_short"), k.Victim.DeathFame, k.Victim.AverageItemPower, timeString), false)
+				embed.Field(fmt.Sprintf("[%v](https://albiononline.com/ru/killboard/kill/%v)", k.Victim.Name, k.EventID), fmt.Sprintf(ctx.Loc("albion_kill_short"), k.Victim.DeathFame, k.Victim.AverageItemPower, timeString), false)
 			}
 			embed.Send(ctx)
 		}
