b20be9b11ff100aeaeace2953162af909d6b453b
FlameInTheDark <nin-vain@mail.ru>
2019-05-18 23:39:09 +0500

    fix

--- bot/albion.go
+++ bot/albion.go
@@ -354,7 +354,7 @@ func (u *AlbionUpdater) Update(session *discordgo.Session, worker *DBWorker, con
 			kills, err := AlbionGetPlayerKills(p.PlayerID)
 			if err != nil {
 				worker.Log("albion", "", fmt.Sprintf("Getting player kills error: %v", err.Error()))
-				fmt.Println("Getting player kills error: %v", err.Error())
+				fmt.Println("Getting player kills error: ", err.Error())
 				return
 			}
 			var newKillTime int64
